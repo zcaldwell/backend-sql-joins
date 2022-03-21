55b7db97e9e05f571a11d0b9d807472271130743
zcaldwell <mintyst.john@gmail.com>
2022-03-21 14:16:31 -0700

    number 7 done

--- exercise_5/exercise-5.sql
+++ exercise_5/exercise-5.sql
@@ -1,2 +1,11 @@
 -- find all film titles that an actor with the last_name Davis appears in
 -- Similar to exercise 3 but with actor, film_actor, film
+SELECT 
+film.title
+FROM
+film
+LEFT JOIN film_actor
+ON film.film_id = film_actor.film_id
+LEFT JOIN actor
+ON actor.actor_id = film_actor.actor_id
+WHERE last_name = 'Davis'
--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,13 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+SELECT
+film.title
+FROM
+film
+LEFT JOIN inventory
+ON film.film_id = inventory.film_id
+LEFT JOIN rental
+ON inventory.inventory_id = rental.inventory_id
+LEFT JOIN customer
+ON rental.customer_id = customer.customer_id
+WHERE first_name = 'Roberta'
